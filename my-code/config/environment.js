908f0683519b3bccaeefbdb33538853812b08d14
Lucia Bianciotto <lulibian@hotmail.com>
2018-10-30 09:54:35 +0000

    Removed my code file

--- my-code/config/environment.js
+++
@@ -1,7 +0,0 @@
-const dbUri = 'mongodb://localhost/instagram';
-const port = 4000;
-
-module.exports = {
-  dbUri: dbUri,
-  port: port
-};
--- my-code/lib/secureRoute.js
+++
@@ -1,9 +0,0 @@
-function secureRoute(req, res, next) {
-  if (req.session.userId) {
-    next();
-  } else {
-    res.redirect('/login');
-  }
-}
-
-module.exports = secureRoute;
